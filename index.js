f0fae4ece7cd15d498205ae494420821f3ac53c8
jondev <jonmac2454@gmail.com>
2025-03-05 08:55:23 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+        console.log('Button clicked!');
+    });
 }
+
+module.exports = { addingEventListener }; 
+
